19f42d9970396115f0d6107f00cece76841a0847
Your name <omowunmidaud1@gmail.com>
2023-07-29 04:39:35 +0100

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "./Header";
+import Main from "./Main";
+import './home.css'
+
+const Home = () => {
+  return (
+    <>
+
+        <Header />
+
+        <Main />
+
+    </>
+  )
+}
+
+export default Home
