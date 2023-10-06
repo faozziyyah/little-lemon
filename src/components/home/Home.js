79a17faa45339c0dd20e3001da42447333a76f46
Your name <omowunmidaud1@gmail.com>
2023-10-06 10:24:08 +0100

    completed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from "./Header";
-import Main from "./Main";
+import {Main} from "./Main";
 import './home.css'
+import {Testimonial} from './Testimonial';
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
         <Header />
 
         <Main />
+
+        <Testimonial />
 
     </>
   )
--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -67,4 +67,4 @@ const Main = () => {
   )
 }
 
-export default Main
+export {Main}
